d09d9485a35b3a05341955374369e13588e91657
romasketch <dopplerside@gmail.com>
2018-07-15 17:42:29 +0300

    updated diff.js after linter check

--- 01-basic-coding-challenges/00-diff.js
+++ 01-basic-coding-challenges/00-diff.js
@@ -1,30 +1,27 @@
 // Напишите функцию diff, которая принимает два угла (integer), каждый от 0 до 360, и возвращает наименьшую разницу между ними.
 
-// Примеры:
-
-// diff(0, 45) === 45;         // не 315, а 45, потому что 45 меньше
-// diff(0, 180) === 180;
-// diff(0, 190) === 170;       // не 190, а 170, потому что 170 меньше
-// diff(120, 280) === 160;
-
+// BEGIN (write your solution here) (write your solution here)
 const diff = (a, b) => {
-    let c = Math.abs(a-b);
-    if(c < 180) {
-        return console.log(c);
+    const c = Math.abs(a - b);
+    if (c < 180) {
+      return c;
     } else if (c > 180) {
-        return console.log(360-c);
+      return 360 - c;
     } else if (c === 180) {
-        return console.log(180);
+      return 180;
     } else if (c === 0) {
-        return console.log(0);
+      return 0;
     }
-} 
+    return false;
+  };
+  // END
+//   export default diff;
 
-diff(0, 45); // 45
-diff(0, 180); // 180
-diff(0, 190); // 170
-diff(120, 280); // 160
-diff(180, 180); // 0
-diff(0, 0); // 0
-diff(360, 240); // 0
-diff(180, 0); // 180
+console.log(diff(0, 45)); // 45
+console.log(diff(0, 180)); // 180
+console.log(diff(0, 190)); // 170
+console.log(diff(120, 280)); // 160
+console.log(diff(180, 180)); // 0
+console.log(diff(0, 0)); // 0
+console.log(diff(360, 240)); // 120
+console.log(diff(180, 0)); // 180
