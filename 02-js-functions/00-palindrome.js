99b516c7d4005c4ade02500568e72a08240d4ad3
romasketch <dopplerside@gmail.com>
2018-07-16 13:07:25 +0300

    Solved palindrom with recursion

--- 02-js-functions/00-palindrome.js
+++ 02-js-functions/00-palindrome.js
@@ -3,12 +3,30 @@ const isPalindrome = (str) => {
   if (str.length < 2) {
     return true;
   }
-  // recursion
+
   if (str[0] === str[str.length - 1]) {
     return isPalindrome(str.substr(1, (str.length - 2)));
   }
   return false;
 };
 
+console.log(isPalindrome('444'));
 
-console.log(isPalindrome('404'));
+// const isPalindrome = (str) => {
+//     // guard expression
+//     if (str.length < 2) {
+//       return true;
+//     }
+    
+//     const startPoint = str[0];
+//     const endPoint = str[str.length - 1];
+  
+//     // second guard expression
+//     if (startPoint !== endPoint) {
+//       return false;
+//     }
+    
+//     // recursion
+//     const cutTheString = str.substr(1, (str.length - 2));
+//     return isPalindrome(cutTheString);
+//   };
